3baa608dad83abd78a590fd850b7a47c3b7aac17
naldurien <nicole.aldurien@gmail.com>
2021-06-28 11:35:20 -0400

    first commit

---
+++ script.rb
@@ -0,0 +1,43 @@
+my_num = 25
+
+my_boolean = true
+
+my_string = "Ruby"
+
+puts my_num
+puts my_boolean
+puts my_string
+
+my_num = 100
+
+puts my_num
+
+# single line comments in Ruby look like this
+
+=begin
+multi-line comments in Ruby
+will look like this
+=end
+
+# a little arithmetic practice
+puts 25%7
+puts 25%5
+puts 2**3
+
+=begin 
+puts vs print:
+puts is short for "put string".
+it also adds a newline after whatever
+you have printed.
+neither need parentheses or semicolons like python or javascript
+=end
+
+#everything in ruby is an object, which allows for things to have built-in methods
+
+puts "Hello. My name is Inigo Montoya. You killed my father. Prepare to die.".length
+
+puts "racecar".reverse
+puts "banana".reverse
+
+puts "hush".upcase
+puts "Tiny".downcase
