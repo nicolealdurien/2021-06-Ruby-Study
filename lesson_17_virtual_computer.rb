b824572d96b9a8915b3c865d5be570fdd2722246
naldurien <nicole.aldurien@gmail.com>
2021-07-07 16:39:06 -0400

    17

---
+++ lesson_17_virtual_computer.rb
@@ -0,0 +1,28 @@
+=begin
+Lesson 16: Object-Oriented Programming I
+(all lessons from Codecademy's Ruby curriculum)
+=end
+
+class Computer
+
+  @@users = {}
+
+  def initialize(username, password)
+      @username = username
+      @password = password
+      @files = {}
+      @@users[username] = password
+  end
+
+  def create(filename)
+    time = Time.now
+    @files[filename] = time
+    puts "#{filename} was created by #{@username} at #{time}"
+  end
+
+  def Computer.get_users
+    return @@users
+  end
+end
+
+my_computer = Computer.new("justjack", "clownshoes")
